aa7aebf12a1fb51ef4cc3c8c38aeb13a01111234
jinri yoo <zzznly@gmail.com>
2024-02-10 03:07:53 +0900

    파일기반 라우팅

---
+++ pages/course/[id]-example.vue
@@ -0,0 +1,10 @@
+<template>
+  <q-page padding>
+    <h2>Course Example</h2>
+    <div class="text-subtitle1">{{ $route.params.id }}-example</div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
