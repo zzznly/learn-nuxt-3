1d316cab2d9f7fce2efeebe4d6e418c146f9127b
jinri yoo <zzznly@gmail.com>
2024-02-10 01:19:45 +0900

    chore: 초기세팅

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  typescript: {
+    shim: false,
+    typeCheck: true,
+  },
+  modules: ['nuxt-quasar-ui'],
+});
