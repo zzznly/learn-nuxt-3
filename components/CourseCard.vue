2aa4172c2180555d51e45d86fac90597db487c34
jinri yoo <zzznly@gmail.com>
2024-02-10 23:46:31 +0900

    course 데이터 바인딩

--- components/CourseCard.vue
+++ components/CourseCard.vue
@@ -1,11 +1,10 @@
 <template>
   <q-card class="my-card" @click="$emit('click')">
-    <img src="https://i.ibb.co/XbsPPmQ/image.png" />
-
+    <img :src="thumbnail" />
     <q-card-section>
-      <div class="text-h6 ellipsis">Vue3 완벽 마스터 기본편</div>
+      <div class="text-h6 ellipsis">{{ title }}</div>
       <div class="text-subtitle2 ellipsis text-grey-8">
-        Vue 3 전체적인 스펙을 깊이있게 다뤄요
+        {{ subtitle }}
       </div>
     </q-card-section>
   </q-card>
