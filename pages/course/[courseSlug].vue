193f862583ba7d7c2556f0e2e9311f1079d93629
jinri yoo <zzznly@gmail.com>
2024-02-11 03:47:29 +0900

    중첩라우팅

--- pages/course/[courseSlug].vue
+++ pages/course/[courseSlug].vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="q-pa-xl">
+  <div>
     <AppCard>
       <template #header>
         <div class="text-h5 text-weight-medium">{{ course?.title }}</div>
@@ -45,6 +45,26 @@
       <p class="q-mt-lg text-grey-8">
         {{ course?.content }}
       </p>
+      <q-separator class="q-mb-lg" />
+      <q-form class="q-gutter-y-md">
+        <q-btn
+          label="수강완료"
+          class="full-width"
+          color="green"
+          unelevated
+          :outline="completed ? false : true"
+          :icon="completed ? 'check' : undefined"
+          @click="completed = !completed"
+        /><q-input
+          v-model="memo"
+          type="textarea"
+          outlined
+          dense
+          placeholder="메모를 작성해주세요."
+          rows="3"
+          autogrow
+        />
+      </q-form>
       <template #footer>
         <ClientOnly>
           <q-btn
@@ -86,8 +106,11 @@ definePageMeta({
   // title: title.value,
   title: 'My Home Page',
   pageType: '',
+  keepalive: true,
+  alias: ['/lecture/:courseSlug'],
 });
-console.log('route.meta.title', route.meta);
+const memo = ref('');
+const completed = ref(false);
 </script>
 
 <style scoped></style>
