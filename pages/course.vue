e24317547d8540b220e1c313bf94c94553dcd1b4
jinri yoo <zzznly@gmail.com>
2024-02-15 17:26:24 +0900

    Revert "delete: Prefetch test"
    This reverts commit 91a2b6c6fa2e748a0d187f6d892fa26db5abeb7b.

--- pages/course.vue
+++ pages/course.vue
@@ -16,6 +16,15 @@
                 {{ index + 1 }}. {{ course.title }}
               </q-item-section>
             </q-item>
+            <NuxtLink v-slot="{ navigate }" custom to="/course/prefetching-1">
+              <q-item clickable @click="navigate()">Prefetching Test 1</q-item>
+            </NuxtLink>
+            <NuxtLink v-slot="{ navigate }" custom to="/course/prefetching-2">
+              <q-item clickable @click="navigate()">Prefetching Test 2</q-item>
+            </NuxtLink>
+            <NuxtLink v-slot="{ navigate }" custom to="/course/prefetching-3">
+              <q-item clickable @click="navigate()">Prefetching Test 3</q-item>
+            </NuxtLink>
           </q-list>
         </q-card>
       </div>
