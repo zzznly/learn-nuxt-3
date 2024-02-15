06d025915d44809dc63a6713903a3fb9c6ea1b7d
jinri yoo <zzznly@gmail.com>
2024-02-15 16:18:10 +0900

    NuxtLink prefetching test

---
+++ pages/course/prefetching-3.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page padding>
+    <div class="text-center q-py-xl">
+      <div class="text-h4">{{ $route.path }}</div>
+    </div>
+  </q-page>
+</template>
+
+<script lang="ts">
+console.log('prefetching-3');
+</script>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
